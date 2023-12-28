7e34fd2bc00f77017faf964ba30aebdeab928e88
DankArmin <armangaspy@gmail.com>
2023-12-28 01:27:47 +0100

    add basic test level

--- prefabs/player/player.gd
+++ prefabs/player/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody3D
 @export var sensitivity: float = 0.03
 
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const JUMP_VELOCITY = 6.0
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
